b49876818647914f68170c711954d04e9456764a
rednaxela700 <ohleg@windowslive.com>
2019-11-24 16:56:05 +0100

    added hooks and inputChange handler

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
-import React from 'react';
+import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const [searchTerm, setSearchTerm] = useState('');
+  const handleChange = event => {setSearchTerm(event.target.value)}
+
   return (
     <div className="App">
       <input
         type="text"
         placeholder="Search"
+        value={searchTerm}
+        onChange={handleChange}
       />
       <ul>
         <li>Item 1</li>
